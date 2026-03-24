402e42654d27f057bb9a32ccd5c9353a1657798b
Louis <79896040+LouisDev314@users.noreply.github.com>
2026-03-24 07:29:37 -0600

    fix: env config

--- configs/env.ts
+++ configs/env.ts
@@ -4,7 +4,7 @@ const envConfig = {
   stripeSecretKey: process.env.STRIPE_SECRET_KEY || '',
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
   supabasePublishableDefaultKey: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY || '',
-  isSiteOpen: process.env.NEXT_PUBLIC_IS_SITE_OPEN === 'true',
+  isSiteOpen: Boolean(process.env.NEXT_PUBLIC_IS_SITE_OPEN === 'true') || true,
 };
 
 const getEnvConfig = () => {
