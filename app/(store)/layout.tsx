cd4d0a31aa7fbb6bd03305953827f5aeeee4f681
Louis <79896040+LouisDev314@users.noreply.github.com>
2026-03-24 07:23:33 -0600

    chore: block prod

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,12 +1,23 @@
 import { StoreHeader } from '@/components/layout/store-header';
 import { StoreFooter } from '@/components/layout/store-footer';
 import { Suspense, type ReactNode } from 'react';
+import getEnvConfig from '@/configs/env';
 
 interface IStoreLayoutProps {
   children: ReactNode;
 }
 
 export default function StoreLayout(props: IStoreLayoutProps) {
+  if (!getEnvConfig().isSiteOpen) {
+    return (
+      <main>
+          <div className="flex min-h-screen items-center justify-center text-xl">
+        🚧 Coming Soon
+          </div>
+      </main>
+    );
+  }
+
   return (
     <>
       <Suspense fallback={<div aria-hidden="true" className="h-16" />}>
